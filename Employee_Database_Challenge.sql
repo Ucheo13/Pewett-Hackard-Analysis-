801db102ad7ba145486194d84302ff9b24c1705c
Ucheo13 <96452277+Ucheo13@users.noreply.github.com>
2022-02-21 03:08:57 -0500

    Add files via upload

--- Employee_Database_Challenge.sql
+++ Employee_Database_Challenge.sql
@@ -1,4 +1,44 @@
-SELECT * FROM mentorship_eligibility 
+
+-- Joining retirement_info and dept_emp tables
+
+SELECT retirement_titles.emp_no,
+    retirement_titles.first_name,
+retirement_titles.last_name,
+	titles.title,
+    titles.from_date,
+	titles.to_date
+INTO retirement_joined_titles
+FROM retirement_titles
+LEFT JOIN titles
+ON retirement_titles.emp_no = titles.emp_no;
+
+
+
+SELECT DISTINCT ON (employees.emp_no) 
+employees.emp_no,
+    employees.first_name,
+employees.last_name,
+	employees.birth_date,
+    dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM employees
+INNER JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+INNER JOIN titles
+ON dept_emp.emp_no = titles.emp_no
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+
+
+
+
+
+
+
+
 
 
 
